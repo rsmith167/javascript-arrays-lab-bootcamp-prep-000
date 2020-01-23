aa9f428e15eedc732868fada5ca2e83d90fe367c
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 15:55:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(existingArray, name){
-  existingArray.push(name)
-  var destructivelyAppendKitten = existingArray
-  return destructivelyAppendKitten
-}
